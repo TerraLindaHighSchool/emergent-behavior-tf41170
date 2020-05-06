7c3415885fa1c32f228f0f0a09791f68d660b78c
tf41170 <60672265+tf41170@users.noreply.github.com>
2020-05-05 23:43:01 -0700

    completed
    Ants collec

--- ants/Ant.java
+++ ants/Ant.java
@@ -8,12 +8,18 @@ import greenfoot.*;
  */
 public class Ant extends Creature
 {
+    private boolean carryingFood =  false;
+    private GreenfootImage image1;
+    private GreenfootImage image2;
+    
      /**
      * Create an ant with a given home hill. The initial speed is zero (not moving).
      */
     public Ant(AntHill home)
     {
         setHomeHill(home);
+        image1 = getImage();
+        image2 = new GreenfootImage("ant-with-food.gif");
     }
     
     /**
@@ -21,17 +27,54 @@ public class Ant extends Creature
      */
     public void act()
     {
-        testFoodPile();
-        randomWalk();
+        status();
     }
     
-    private void testFoodPile()
+    private void checkForFood()
     {
         Food food = (Food) getOneIntersectingObject(Food.class);
         if (food != null) 
         {
             food.removeCrumb();
+            carryingFood = true;
+            setImage(image2);
         }
     }
     
+    private boolean atHome()
+    {
+        if (getHomeHill() != null)
+        {
+            return (Math.abs(getX() - getHomeHill().getX()) < 4) &&
+                   (Math.abs(getY() - getHomeHill().getY()) < 4);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    private void searchForFood()
+    {
+        randomWalk();
+        checkForFood();
+    }
+    
+    private void status()
+    {
+        if (carryingFood == true)
+        {
+            walkTowardsHome();
+            if(atHome())
+            {
+                setImage(image1);
+                carryingFood = false;
+                getHomeHill().countFood();
+            }
+        }
+        else
+        {
+            searchForFood();
+        }
+    }
 }
