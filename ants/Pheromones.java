324f7a2f83d07bf86c68ecd6637ed1d660db7b63
tf41170 <60672265+tf41170@users.noreply.github.com>
2020-05-08 23:51:38 -0700

    completed
    Ants - Adding Pheromones

---
+++ ants/Pheromones.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pheromones here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pheromones extends Actor
+{
+    private GreenfootImage image;
+    private final int MAX_INTENSITY = 180;
+    private int intensity;
+    
+    public Pheromones()
+    {
+        intensity = MAX_INTENSITY;
+    }
+    
+    /**
+     * Act - do whatever the Pheromones wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        intensity--;
+        if(intensity <= 0)
+        {
+            // to be implemented
+        }
+        else
+        {
+             if ((intensity % 6) == 0)  // prevents updating too often
+             { 
+                 updateImages();
+             }
+        }
+    }    
+    
+    private void updateImages()
+    {
+        int size = intensity / 3+5;
+        image = new GreenfootImage(size + 1, size + 1);
+        image.setColor(new Color(225, 225, 225, intensity / 3));
+        image.fillOval(0, 0, size, size);
+        image.setColor(Color.GRAY);
+        image.fillOval(size/2, size/2, 2, 3);
+        setImage(image);
+    }
+}
